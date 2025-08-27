2dc66ba0f74c917f6a72c2d26113e6a192ae927d
aruana08 <shaimurataruana@gmail.com>
2025-08-26 20:32:45 -0500

    Tweaked project

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -12,18 +12,17 @@ export const HeroSection = () => {
             <span className="opacity-0 animate-fade-in"> Hi, I'm</span>
             <span className="text-primary opacity-0 animate-fade-in-delay-1">
               {" "}
-              Pedro
+              Aruana
             </span>
             <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2">
               {" "}
-              Machado
+              Shaimurat
             </span>
           </h1>
 
           <p className="text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">
-            I create stellar web experiences with modern technologies.
-            Specializing in front-end development, I build interfaces that are
-            both beautiful and functional.
+            i create stellar web experiences with modern technologies.
+            i'm interested in creating aesthetic full-stack applications!
           </p>
 
           <div className="pt-4 opacity-0 animate-fade-in-delay-4">
